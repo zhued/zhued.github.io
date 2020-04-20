0a2f27c68f44689365452853ad4e17b40c5a3d24
zhued <edward.r.zhu@gmail.com>
2020-04-20 14:38:03 -0600

    moved layout to own dir

--- src/components/layouts/layout.js
+++ src/components/layouts/layout.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+import Header from "../header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -35,9 +35,7 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          © {new Date().getFullYear()}
         </footer>
       </div>
     </>
