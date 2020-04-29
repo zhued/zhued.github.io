5853daf38e9adb5dd8591fd20c8cacd5a8ffa4af
zhued <edward.r.zhu@gmail.com>
2020-04-28 21:19:01 -0600

    header svg updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,8 +10,18 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
       },
+    },
+    {
+      resolve: 'gatsby-plugin-module-resolver',
+      options: {
+        root: './src', // <- will be used as a root dir
+        aliases: {
+          '@components': './components', // <- will become ./src/components
+          '@icons': './assets/icons', // <- will become ./src/assets.icons
+        }
+      }
     },
     `gatsby-plugin-styled-components`,
     `gatsby-transformer-sharp`,
@@ -25,7 +35,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,21 @@ import PropTypes from "prop-types"
 import React from "react"
 import styled from "styled-components"
 
+import Sun from "@icons/Sun";
+import Moon from "@icons/Moon";
+
 const HeaderContainer = styled.div`
   max-width: 1200px;
   height: 20px;
 `
 
 const ToggleTheme = styled.div`
-  float: right
+  float: right;
+  opacity: 0.3;
+
+  &:hover {
+    opacity: 1;
+  }
 `
 
 const Header = ({ toggleTheme, isLightTheme }) => (
@@ -19,7 +27,7 @@ const Header = ({ toggleTheme, isLightTheme }) => (
       {siteTitle}
     </Link> */}
     <ToggleTheme onClick={toggleTheme}>
-      {isLightTheme ? "light" : "dark"}
+      {isLightTheme ? <Sun/> : <Moon/>}
     </ToggleTheme>
 
   </HeaderContainer>
